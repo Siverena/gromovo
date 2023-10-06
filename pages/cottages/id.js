bae553ef33bf3e6dad677c8ba9b3cb2313a4161b
Siverena <vpupkin@company.com>
2023-10-06 12:50:58 +0300

    bugfix

--- pages/cottages/id.js
+++ pages/cottages/id.js
@@ -3,29 +3,29 @@ import { mapState, mapActions } from 'pinia';
 import { useCottagesStore } from '@/stores/cottagesStore.js';
 
 export default {
-  data() {
-    return {
-      loading: true,
-    };
-  },
-  computed: {
-    ...mapState(useCottagesStore, ['getCottage']),
-  },
-  watch: {},
-  methods: {
-    ...mapActions(useCottagesStore, ['fetchCottage']),
-    async loadData() {
-      try {
-        this.loading = true;
-        await this.fetchCottage(this.$route.params.id);
-        this.loading = false;
-      } catch (e) {
-        console.log(e);
-      }
+    data() {
+        return {
+            loading: true,
+        };
     },
-    getBooking() {},
-  },
-  async created() {
-    await this.loadData();
-  },
+    computed: {
+        ...mapState(useCottagesStore, ['getCottage']),
+    },
+    watch: {},
+    methods: {
+        ...mapActions(useCottagesStore, ['fetchCottage']),
+        async loadData() {
+            try {
+                this.loading = true;
+                await this.fetchCottage(this.$route.params.id);
+                this.loading = false;
+            } catch (e) {
+                console.log(e);
+            }
+        },
+        getBooking() {},
+    },
+    async created() {
+        await this.loadData();
+    },
 };
